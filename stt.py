4e2680ff24e31a837a12e6c6722bd8f753d53308
Brian Barragan <bbarragancid23@uchicago.edu>
2025-07-30 13:31:44 -0500

    Mac and Linux can now run

--- stt.py
+++ stt.py
@@ -1,24 +1,22 @@
-import torch
 from pyannote.audio import Model, Inference, Pipeline
-import os, time
+import os, time, subprocess, torch, platform
 from dotenv import load_dotenv
 import speech_recognition as sr
 from scipy.spatial.distance import cdist
-import numpy as np
-import subprocess   
+import numpy as np 
 from colorama import Fore, Style, init
 
-init()
+init() # so that windows can detect the colors
 
 load_dotenv()
 
 class WhisperSTT:
     def __init__(self, user_name, inference_file, user_num):
-        self.device = torch.device("cpu")
+        self.device = torch.device("cpu")   # Change to "cuda" if you have an NVIDIA GPU
         embeddingmodel = Model.from_pretrained("pyannote/embedding", cache_dir="models/pyannote", 
                                                use_auth_token=os.getenv("HF_API_KEY"))
+        
         self.inference = Inference(embeddingmodel, window="whole", device=self.device)
-
         self.main_speaker_embedding = self.inference(inference_file)
         self.user_name = user_name
         self.user_num = user_num
@@ -36,13 +34,12 @@ class WhisperSTT:
             start = time.time()
             output = self.process_audio(transcribed_file)
             end = time.time()
-            #print(f"[{self.user_name}] Listen offline transcription in {end - start} seconds")
             clean_output = str(output).replace(" ", "").replace(".", "").replace(",", "").lower()
             if clean_output != "you" and clean_output != "":
                 print("(Whisper)", self.color + f"{self.user_name} said: {output}" + Style.RESET_ALL)
 
-        # except sr.UnknownValueError:
-        #     print("Speech Recognition could not understand you")
+        except sr.UnknownValueError:
+            print("Speech Recognition could not understand you")
 
         except sr.RequestError as e:
             print('Could not request results from Google speech recognition service ')
@@ -89,20 +86,25 @@ class WhisperSTT:
         if not os.path.exists(wav_file):
             raise FileNotFoundError(f"WAV file not found {wav_file}")
         
-        executable = os.path.abspath(
-            os.path.join("modules", "whisper.cpp", "build", "bin", "Release", "whisper-cli.exe")
-        )
+        system = platform.system()
+
+        if system == "Windows":
+            executable = os.path.abspath(
+                os.path.join("modules", "whisper.cpp", "build", "bin", "Release", "whisper-cli.exe")
+            )
+        else:  # macOS or Linux
+            executable = os.path.abspath(
+                os.path.join("modules", "whisper.cpp", "main")
+            )
+
         full_command = [executable, "-m", model, "-f", wav_file, "-np", "-nt", "-fa", "-l", "en"]
-
         process = subprocess.Popen(full_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
         output, error = process.communicate()
 
         if error:
             raise Exception(f"Error parsing audio: {error.decode('utf-8')}")
         
         decoded_str = output.decode("utf-8").strip()
-
         processed_str = decoded_str.replace('[BLANK_AUDIO]', "").replace("[BLANK_AUDIO] ,", "").replace("[INAUDIBLE]", "").replace('[SILENCE]', "").replace('[SILENCE] ,', '').replace('[INAUDIBLE] ,', "").strip()
 
         return processed_str
